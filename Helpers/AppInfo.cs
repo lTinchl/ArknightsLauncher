04f85909d8ed7801520bed158d79e98948837904
lTinchl <1138808846@qq.com>
2026-03-19 21:10:31 +0800

    Version change

--- Helpers/AppInfo.cs
+++ Helpers/AppInfo.cs
@@ -4,7 +4,7 @@ namespace ArknightsLauncher.Helpers
 {
     public static class AppInfo
     {
-        public static readonly Version CurrentVersion = new Version("1.3.5.5");
+        public static readonly Version CurrentVersion = new Version("1.3.5.6");
 
         public const string GitHubUrl        = "https://github.com/lTinchl/ArknightsLauncher";
         public const string QuarkPanUrl      = "https://pan.quark.cn/s/eb6c9c0e45df";
